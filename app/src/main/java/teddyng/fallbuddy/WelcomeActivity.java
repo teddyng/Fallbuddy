73e8f83ef58328588a8d422dc7438a1635dd23d0
teddyng <teddyng@outlook.com>
2019-06-10 08:54:00 +0100

    added accelerometer

---
+++ app/src/main/java/teddyng/fallbuddy/WelcomeActivity.java
@@ -0,0 +1,28 @@
+package teddyng.fallbuddy;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        Button button = (Button) findViewById(R.id.startbtn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
+    
--- app/src/main/java/teddyng/fallbuddy/scanner.java
+++ app/src/main/java/teddyng/fallbuddy/scanner.java
@@ -1,28 +1,16 @@
 package teddyng.fallbuddy;
 
-import android.Manifest;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.net.Uri;
-import android.os.Build;
-import android.os.IBinder;
-import android.provider.Settings;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import com.mbientlab.metawear.Data;
 import com.mbientlab.metawear.MetaWearBoard;
@@ -39,16 +27,7 @@ import com.mbientlab.metawear.module.Accelerometer;
 import bolts.Continuation;
 import bolts.Task;
 
-public class MainActivity extends AppCompatActivity implements ServiceConnection {
-
-    //private Button button;
-    private Button map;
-    private TextView latitude;
-    private TextView longitude;
-    private LocationManager locationManager;
-    private LocationListener locationListener;
-    double latString;
-    double longString;
+public class scanner extends AppCompatActivity implements ServiceConnection {
 
     private BtleService.LocalBinder serviceBinder;
     private MetaWearBoard board;
@@ -57,7 +36,7 @@ public class MainActivity extends AppCompatActivity implements ServiceConnection
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_scanner);
 
         // Bind the service when the activity is created
         getApplicationContext().bindService(new Intent(this, BtleService.class),
@@ -83,65 +62,6 @@ public class MainActivity extends AppCompatActivity implements ServiceConnection
                 board.tearDown();
             }
         });
-
-        //button = (Button) findViewById(R.id.button);
-        map = (Button) findViewById(R.id.mapbtn);
-        latitude = (TextView) findViewById(R.id.latitudeview);
-        longitude = (TextView) findViewById(R.id.longitudeview);
-
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                latString = location.getLatitude();
-                longString = location.getLongitude();
-                String latStringView = "Latitude: " + location.getLatitude();
-                String longStringView = "Longitude: " + location.getLongitude();
-                latitude.setText(latStringView);
-                longitude.setText(longStringView);
-
-                final String latLong = latString + "," + longString;
-
-                map.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Uri gmmIntentUri = Uri.parse("geo:0,0?q=" + latLong + "(Your Location)");
-                        //Uri gmmIntentUri = Uri.parse(latLong);
-                        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-                        mapIntent.setPackage("com.google.android.apps.maps");
-                        startActivity(mapIntent);
-                        //Log.d("Coordination", latString + " " + longString);
-                    }
-                });
-            }
-
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String provider) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String provider) {
-                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivity(intent);
-            }
-        };
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(new String[]{Manifest.permission.INTERNET, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 10);
-                return;
-            }
-        }else{
-            configureButton();
-        }
-
-        locationManager.requestLocationUpdates("gps", 15000, 0, locationListener);
 
     }
 
@@ -220,32 +140,4 @@ public class MainActivity extends AppCompatActivity implements ServiceConnection
             }
         });
     }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode){
-            case 10:
-                if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                    configureButton();
-                    return;
-        }
-    }
-
-    private void configureButton() {
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                locationManager.requestLocationUpdates("gps", 15000, 0, locationListener);
-//            }
-//        });
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(new String[]{Manifest.permission.INTERNET, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 10);
-                return;
-            }
-        }
-        locationManager.requestLocationUpdates("gps", 15000, 0, locationListener);
-    }
 }
-
